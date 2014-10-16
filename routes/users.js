249bc678565333c135042524cf8918309211aeb9
HmsChumsai <ravipong.chumsai@gmail.com>
2014-10-16 05:40:08 +0000

    New Stock Panel

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,56 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET Stock Chart. */
 router.get('/', function(req, res) {
     var collection = req.collection;
+    var stockname=req.body.stockname;
+    if (!stockname) {stockname="ILINK"};
     var data = [];
     collection.find({
         name: "ILINK"
+    }).sort({
+        _id: 1
+    }).toArray(function(err, items) {
+        //data = items;
+
+        console.log("find some items");
+        console.log(items.length);
+        for (var i = 0; i < items.length; i++) {
+            //for (var i = 0; i < 20; i++) {
+            var EOD = {
+                date: "",
+                open: "",
+                high: "",
+                low: "",
+                close: ""
+            };
+
+            EOD.date = items[i].date;
+            EOD.open = "" + items[i].open;
+            EOD.high = "" + items[i].high;
+            EOD.low = "" + items[i].low;
+            EOD.close = "" + items[i].close;
+            //console.log(JSON.stringify(EOD));
+            data[i] = EOD;
+
+        }
+
+        res.render('test', {
+            "data": data
+        });
+
+    });
+
+});
+
+router.get('/:id', function(req, res) {
+    var collection = req.collection;
+    var stockname=req.params.id;
+    if (!stockname) {stockname="ILINK"};
+    var data = [];
+    collection.find({
+        name: stockname
     }).sort({
         _id: 1
     }).toArray(function(err, items) {
