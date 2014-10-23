e38a6b50c9f780ddeb6453d62db88ebbb6797924
HmsChumsai <ravipong.chumsai@gmail.com>
2014-10-23 09:26:29 +0000

    make it better

--- routes/charts.js
+++ routes/charts.js
@@ -2,81 +2,11 @@ var express = require('express');
 var async = require("async");
 var router = express.Router();
 
+router.get('/', function(req, res, next) {
+    res.render("index.html");
+});
 
-
-/* GET Stock Chart. */
 /*
-router.get('/', function(req, res) {
-
-console.log("Path / reg.params=" + JSON.stringify(req.params));
-console.log("Path / reg.body=" + JSON.stringify(req.body));
-console.log("Path / reg.query=" + JSON.stringify(req.query));
-
-var collection = req.collection;
-var stockname = req.query.stockname;
-if (!stockname) {
-stockname = "SET"
-};
-
-var data=[];
-var second=[];
-var third = [];
-collection.find({
-name: stockname
-}).sort({
-date: 1
-}).toArray(function(err, items) {
-
-//data = items;
-
-
-console.log("find some items");
-console.log(items.length);
-for (var i = 0; i < items.length; i++) {
-
-data.push(items[i]);
-}
-collection.find({
-name: "ADVANC"
-}).sort({
-date: 1
-}).toArray(function(err, items) {
-
-//data = items;
-console.log("find some items");
-console.log(items.length);
-for (var i = 0; i < items.length; i++) {
-
-second.push(items[i]);
-}
-collection.find({
-name: "AOT"
-}).sort({
-date: 1
-}).toArray(function(err, items) {
-
-//data = items;
-console.log("find some items");
-console.log(items.length);
-for (var i = 0; i < items.length; i++) {
-
-third.push(items[i]);
-}
-console.log(JSON.stringify(third));
-res.render('charts', {
-"data": data,
-"stockname": stockname,
-"second":second,
-"third":third
-});
-});
-
-});
-
-});
-
-});
-*/
 
 router.get('/', function(req, res, next) {
 
@@ -106,7 +36,7 @@ router.get('/', function(req, res, next) {
                     callback();
                 });
             }
-        */
+        
         , function(err) { //This function gets called after the two tasks have called their "task callbacks"
             if (err) return next(err);
             console.log(JSON.stringify(results));
@@ -141,5 +71,7 @@ router.get('/', function(req, res, next) {
         }
     };
 });
+
+*/
 
 module.exports = router;
